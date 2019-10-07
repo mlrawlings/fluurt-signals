ce03e4bfc0a33689e291ad0d0c75e6fc8abc26c5
Dylan Piercey <dpiercey@ebay.com>
2019-10-07 10:51:32 -0700

    feat: add helpers for html rendering

--- html/index.ts
+++ html/index.ts
@@ -2,6 +2,10 @@ export { register } from "../common/registry";
 
 export { classAttr, styleAttr } from "../common/helpers";
 
+export { escapeScript, escapeStyle, escapeXML } from "./escape";
+
+export { attr, attrs } from "./attrs";
+
 export {
   createRenderer,
   write,
